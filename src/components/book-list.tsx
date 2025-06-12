07c3edfe53e3552d08e9931899c758ef7ed11c88
Hilda Kh Amiri <hilda.kh.amiri@gmail.com>
2025-06-12 10:56:17 +0400

    fixup

--- src/components/book-list.tsx
+++ src/components/book-list.tsx
@@ -22,7 +22,7 @@ export default function BookList() {
 
   return (
     <div className="w-3/4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {books.map((book) => (
+      {books?.map((book) => (
         <Card key={book.id} book={book} />
       ))}
     </div>
