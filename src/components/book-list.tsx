5ae7b56be1d89a18698bbf7c6f9623f3e1dd40d3
Hilda Kh Amiri <hilda.kh.amiri@gmail.com>
2025-06-12 05:32:09 +0400

    Update booklist to display error & loading UI - Update card styling

--- src/components/book-list.tsx
+++ src/components/book-list.tsx
@@ -1,22 +1,30 @@
-"use client";
+'use client';
 
-import { Book, useBooks } from "@/hooks/useBooks";
+import { useBooks } from "@/hooks/useBooks";
 import { useTranslations } from "next-intl";
 import Card from "./card";
 
 export default function BookList() {
-  const { books, loading } = useBooks();
+  const { books, loading, error } = useBooks();
   const t = useTranslations('BookList');
 
+  if (loading) {
+    return <p className="text-center">{t('loading')}</p>;
+  }
+
+  if (error) {
+    return (
+      <div className="text-red-600 px-4 py-3 rounded-lg bg-red-50">
+        {error}
+      </div>
+    );
+  }
+
   return (
-    <div className="w-3/4">
-      {loading ? (
-        <p>{t('loading')}</p>
-      ) : (
-        books.map((book: Book) => (
-          <Card key={book.id} book={book} />
-        ))
-      )}
+    <div className="w-3/4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {books.map((book) => (
+        <Card key={book.id} book={book} />
+      ))}
     </div>
-  )
+  );
 }
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,17 +1,27 @@
 import { Book } from "@/hooks/useBooks";
 
 interface Props {
-  book: Book,
+  book: Book
 }
 
 export default function Card({ book }: Props) {
   return (
-    <div
-      className="bg-background border border-primary/10 rounded-xl p-6 shadow-sm"
-    >
-      <p>{book.title}</p>
-      <p>{book.author}</p>
-      <p>{book.price}</p>
+    <div className="bg-background border border-primary/10 rounded-xl p-4 shadow-sm hover:shadow-md hover:scale-102 transition-all duration-200">
+      <h3 className="text-xl font-bold text-foreground/90 mb-3
+                     line-clamp-2">
+        {book.title}
+      </h3>
+
+      <p className="text-foreground/70 mb-4 font-md">
+        {book.author}
+      </p>
+
+      <div className="inline-block bg-primary/10 text-primary
+                      px-3 py-1 rounded-full">
+        <span className="font-semibold">
+          ${book.price.toFixed(2)}
+        </span>
+      </div>
     </div>
   );
-};
+}
